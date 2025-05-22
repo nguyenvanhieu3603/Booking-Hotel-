27ca72664b15bbff82de8192570cc57288f28cfc
nguyenHieu <124961380+nmh2003@users.noreply.github.com>
2025-05-22 13:10:02 +0700

    Add files via upload
    update inc

--- bookingBackend/inc/bootstrap.php
+++ bookingBackend/inc/bootstrap.php
@@ -10,7 +10,7 @@ require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
 // include model files
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
-// require_once PROJECT_ROOT_PATH . "/Model/HotelModel.php";
+require_once PROJECT_ROOT_PATH . "/Model/HotelModel.php";
 // require_once PROJECT_ROOT_PATH . "/Model/RoomModel.php";
 // require_once PROJECT_ROOT_PATH . "/Model/BookingModel.php";  
 // require_once PROJECT_ROOT_PATH . "/Model/PaymentModel.php";  
