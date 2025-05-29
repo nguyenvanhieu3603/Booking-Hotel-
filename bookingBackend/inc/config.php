86b8880f19f970749f360981141b717b8042b6dd
nguyenHieu <124961380+nmh2003@users.noreply.github.com>
2025-05-29 13:33:11 +0700

    Update config.php

--- bookingBackend/inc/config.php
+++ bookingBackend/inc/config.php
@@ -5,6 +5,6 @@ define("DB_PASSWORD", ""); // Mặc định XAMPP không có password
 define("DB_DATABASE_NAME", "hotel_booking");
 define("JWT_SECRET", "your_secret_key_here"); // Cho chức năng JWT sau này
 define('BASE_URL', 'http://localhost/bookingBackend');
-define('PAYPAL_CLIENT_ID', 'AbnKWyEXeoUnlNstxEK-fELJjr2ivMFs5CeEtQcPOG-wJ82WypMDtIuAwKKxRnaIGZIH40tIHNFWkgJd');
-define('PAYPAL_CLIENT_SECRET', 'EBPLEsFEZo2lwK8VbEnOCtx_oGg8_gRTkbWnxL-6UOtPV6oEy1f5EHmVBWPX9vBrQ2C4KxwJJRFmy67b');
+define('PAYPAL_CLIENT_ID', 'abcd');
+define('PAYPAL_CLIENT_SECRET', 'jqk');
 ?>
