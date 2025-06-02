79c34fadfed0e983bf4b80a43b38d62f3a188191
nguyenHieu <124961380+nmh2003@users.noreply.github.com>
2025-06-02 14:09:12 +0700

    Update Hotel Model

--- bookingBackend/Model/HotelModel.php
+++ bookingBackend/Model/HotelModel.php
@@ -3,9 +3,30 @@ require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 
 class HotelModel extends Database
 {
-    public function getHotels($limit = 10)
+    public function getHotels()
     {
-        return $this->select("SELECT * FROM hotels WHERE active != 1 ORDER BY id ASC LIMIT ?", ["i", $limit]);
+        return $this->select("SELECT * FROM hotels WHERE active != 1 ORDER BY id ASC ", ["i"]);
+    }
+
+    public function getAllHotels()
+    {
+        return $this->select("SELECT * FROM hotels ORDER BY id ASC ", ["i"]);
+    }
+
+    public function getHotelsByAddress($address)
+    {
+        return $this->select(
+            "SELECT * FROM hotels WHERE TRIM(SUBSTRING_INDEX(address, ',', -1)) = ? ORDER BY id ASC",
+            ["s", "$address"]
+        );
+    }
+
+    public function getHotelByRating($rating)
+    {
+        return $this->select(
+            "SELECT * FROM hotels WHERE rating >= ? AND active != 1 ORDER BY id ASC",
+            ["d", $rating]
+        );
     }
 
     public function getHotelById($hotelId)
@@ -54,5 +75,33 @@ class HotelModel extends Database
             "SELECT image_url FROM hotel_images WHERE hotel_id = ?",
             ["i", $hotelId]
         );
+    }
+
+    public function deleteHotelImage($hotelId, $imagePath)
+    {
+        return $this->delete(
+            "DELETE FROM hotel_images WHERE hotel_id = ? AND image_url = ?",
+            ["is", $hotelId, $imagePath]
+        );
+        if (file_exists($imagePath)) {
+            unlink($imagePath);
+        }
+    }
+
+    public function countHotelsByProvince($address)
+    {
+        $result = $this->select(
+            "SELECT COUNT(*) as count FROM hotels WHERE TRIM(SUBSTRING_INDEX(address, ',', -1)) = ?",
+            ["s", $address]
+        );
+        return $result[0]['count'] ?? 0;
+    }
+
+    public function filterHotelsByRatingAndProvince($province, $rating)
+    {
+        return $this->select("SELECT * FROM hotels 
+        WHERE active != 1 
+        AND TRIM(SUBSTRING_INDEX(address, ',', -1)) = ?
+        AND rating >= ? ORDER BY id ASC", ["sd", $province, $rating]);
     }
 }
